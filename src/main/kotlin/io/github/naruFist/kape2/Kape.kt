716fcb07d8674a86bfb03ac48d8925ee073b404d
naruFist <hino123456@outlook.kr>
2025-11-04 18:59:01 +0900

    :bug: this is real...

--- src/main/kotlin/io/github/naruFist/kape2/Kape.kt
+++ src/main/kotlin/io/github/naruFist/kape2/Kape.kt
@@ -1,42 +1,51 @@
 package io.github.naruFist.kape2
 
 import org.bukkit.event.Event
-import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
+import org.bukkit.plugin.EventExecutor
 import org.bukkit.plugin.java.JavaPlugin
 
 annotation class KapeDSL
 
-internal lateinit var plugin: JavaPlugin
+private var _plugin: JavaPlugin? = null
+
+var plugin: JavaPlugin
+    get() = _plugin ?: throw IllegalStateException("Plugin not initialized yet!")
+    set(value) {
+        _plugin = value
+    }
 
 
 class Kape {
     companion object {
-        @JvmStatic
-        fun setPlugin(javaPlugin: JavaPlugin) {
-            plugin = javaPlugin
-        }
-
-        @JvmStatic
-        fun plugin() = plugin
-
-        @KapeDSL
-        @JvmStatic
-        fun <E: Event>listener(clazz: Class<E>, block: (event: E) -> Unit) {
-            val listenerBlock = object : Listener {
-                @EventHandler
-                fun a(event: E) {
-                    block.invoke(event)
-                }
-            }
-
-            plugin.server.pluginManager.registerEvents(listenerBlock, plugin)
-        }
 
         @KapeDSL
         @JvmStatic
         inline fun <reified E : Event> listener(noinline block: (E) -> Unit) {
-            listener(E::class.java, block)
+            val dummyListener = object : Listener
+
+            val executor = EventExecutor { _, event ->
+                if (event is E) block(event)
+            }
+
+            plugin.server.pluginManager.registerEvent(
+                E::class.java, dummyListener, EventPriority.NORMAL, executor, plugin, false
+            )
+        }
+
+        @KapeDSL
+        @JvmStatic
+        inline fun <reified E : Event> listener(priority: EventPriority, noinline block: (E) -> Unit) {
+            val dummyListener = object : Listener
+
+            val executor = EventExecutor { _, event ->
+                if (event is E) block(event)
+            }
+
+            plugin.server.pluginManager.registerEvent(
+                E::class.java, dummyListener, priority, executor, plugin, false
+            )
         }
     }
 }
